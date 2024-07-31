1e62cfabe96d621ee6c6db2cb39ee5c135376da4
Jhojan-crypto <67449386+Jhojan-crypto@users.noreply.github.com>
2024-07-31 16:20:23 -0500

    Add files via upload

---
+++ act2-3_par_impar/par_impar.cpp
@@ -0,0 +1,18 @@
+/* Dado un numero determina si es par o impar */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero;
+    cout << "Introduce un numero: ";
+    cin >> numero;
+
+    if (numero % 2 == 0) {
+        cout << numero << " es par." << endl;
+    } else {
+        cout << numero << " es impar." << endl;
+    }
+
+    return 0;
+}
